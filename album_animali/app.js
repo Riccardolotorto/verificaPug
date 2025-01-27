e5fef1e57fcf89761407184e3c4bee9585e6486e
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2025-01-27 08:45:35 +0000

    punto 4 da sistemare

--- album_animali/app.js
+++ album_animali/app.js
@@ -1,16 +1,25 @@
 var express = require('express');
-const animale = require('./animals.json'); 
+const animale = require('./animals.json');
 var app = express();
 app.set('view engine', 'pug'); 
 app.use(express.static(__dirname + '/public')); 
 
 app.get('/', function (req, res) {
-
+    res.render('index', {
+        animale: animale.animals
+    });
 });
 
 app.get('/api/albumAnimali', (req, res) => {
     res.render('index', {
         animale: animale.animals
+    });
+});
+
+app.get('/album', (req, res) => {
+    const animalll = animale.animals.find((p) => p.id_figurina === req.query.id_figurina);  
+    res.render('album', {
+        animalll
     });
 });
 
